b997fe6d2d5a6eba00fe132f238fb452e2c1e661
AlbertOsinaga <luisalberto.osinaga@gmail.com>
2017-08-30 10:17:03 -0400

    LS_MyCashFlow. Parametros, Monedas y Cuentas

---
+++ LS_MyCashFlow/MyCashFlow/MyCashFlow.Server/DataSources/ApplicationData/_ApplicationDataService.lsml.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.LightSwitch.Security.Server;
+using Microsoft.LightSwitch;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+
+namespace LightSwitchApplication
+{
+    public partial class ApplicationDataService
+    {
+        void Cuentas_Changing(Cuenta entity)
+        {
+            entity.Nombre = entity.Nombre.Trim().ToUpper();
+        }
+
+        partial void Cuentas_Inserting(Cuenta entity)
+        {
+            Cuentas_Changing(entity);
+        }
+
+        partial void Cuentas_Updating(Cuenta entity)
+        {
+
+        }
+
+        void Monedas_Changing(Moneda entity)
+        {
+            entity.Nab = entity.Nab.Trim().ToUpper();
+        }
+
+        partial void Monedas_Inserting(Moneda entity)
+        {
+            Monedas_Changing(entity);
+        }
+
+        partial void Monedas_Updating(Moneda entity)
+        {
+            Monedas_Changing(entity);
+        }
+
+        void Parametros_Changing(Parametro entity)
+        {
+            entity.Categoria = entity.Categoria.Trim().ToUpper();
+            entity.Clave = entity.Clave.Trim().ToUpper();
+        }
+
+
+        partial void Parametros_Inserting(Parametro entity)
+        {
+            Parametros_Changing(entity);
+        }
+
+        partial void Parametros_Updating(Parametro entity)
+        {
+            Parametros_Changing(entity);
+        }
+    }
+}
